6509f4eff06aa3f133474ae5805913df1565de3e
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-05-01 14:16:33 +0100

    Add files via upload

---
+++ Python/Area of a Circle/areaofcircle1.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("Enter the radius of the circle in cm: "))
+
+square = radius * radius
+
+area = math.pi * square
---
+++ Python/Area of a Circle/areaofcircle2.py
@@ -0,0 +1,3 @@
+import areaofcircle1
+
+print("The area of a circle of radius ", areaofcircle1.radius, "cm is %.2f" % areaofcircle1.area, "cm²")
